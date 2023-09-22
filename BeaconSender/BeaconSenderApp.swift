38f16f79a470e05ae5bd8ced4d9c3c0b8486a50c
Tomotaka Matsushita <to-matsushita@icd.co.jp>
2023-09-22 09:25:11 +0900

    Initial Commit

---
+++ BeaconSender/BeaconSenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeaconSenderApp.swift
+//  BeaconSender
+//
+//  Created by to-matsushita on 2023/09/22.
+//
+
+import SwiftUI
+
+@main
+struct BeaconSenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
